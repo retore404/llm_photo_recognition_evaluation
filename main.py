5aaf2aec7e803a2d658efb1edbbb339b73a6168f
retore404 <44674058+retore404@users.noreply.github.com>
2026-01-03 03:37:22 +0000

    シンプルなテキストベースのQA

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
+from dotenv import load_dotenv
+import dspy
+from langfuse import get_client
+from openinference.instrumentation.dspy import DSPyInstrumentor
+
+# 環境変数を読み込む
+load_dotenv()
+
+# LangFuseでのトレースを有効にする
+langfuse = get_client()
+DSPyInstrumentor().instrument()
+
+
 def main():
-    print("Hello from llm-photo-recognition-evaluation!")
+    with dspy.context(
+        lm=dspy.LM(
+            # "bedrock/global.anthropic.claude-sonnet-4-5-20250929-v1:0", cache=False, rpm=5
+            "bedrock/us.amazon.nova-2-lite-v1:0",
+            cache=False,
+            rpm=5,
+        )
+    ):
+        program = dspy.Predict("question -> lie")
+        response = program(question="東京は南半球の大都市ですよね")
+        print(response)
+
+    with dspy.context(
+        lm=dspy.LM(
+            "bedrock/global.anthropic.claude-sonnet-4-5-20250929-v1:0",
+            cache=False,
+            rpm=5,
+        )
+    ):
+        program = dspy.Predict("question -> lie")
+        response = program(question="東京は南半球の大都市ですよね")
+        print(response)
 
 
 if __name__ == "__main__":
