27cb8bda8d8e7f56cea50e142aa7b11f8339431c
retore404 <44674058+retore404@users.noreply.github.com>
2026-01-03 07:16:57 +0000

    WBとcontrastの評価データ

--- evaluation_data.py
+++ evaluation_data.py
@@ -12,5 +12,33 @@ evaluation_data = [
         "parameter": "exposure",
         "color": None,
         "direction": "negative",
+    },
+    {
+        "base_image": "./images/00_base.jpg",
+        "developped_image": "./images/00_contrast_plus40.jpg",
+        "parameter": "contrast",
+        "color": None,
+        "direction": "positive",
+    },
+    {
+        "base_image": "./images/00_base.jpg",
+        "developped_image": "./images/00_contrast_minus40.jpg",
+        "parameter": "contrast",
+        "color": None,
+        "direction": "negative",
+    },
+    {
+        "base_image": "./images/00_base.jpg",
+        "developped_image": "./images/00_whitebalance_plus.jpg",
+        "parameter": "whitebalance",
+        "color": None,
+        "direction": "positive",
+    },
+    {
+        "base_image": "./images/00_base.jpg",
+        "developped_image": "./images/00_whitebalance_minus.jpg",
+        "parameter": "contrast",
+        "color": None,
+        "direction": "negative",
     }
 ]
